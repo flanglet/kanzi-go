f565e1ddc5aea2c34421d5b333178101bfc6b61c
fred <flanglet@gmail.com>
2014-01-26 22:41:40 -0800

    Fix bug when trying to remove root. Add contains(), toArray() and clear() methods. Several speed improvements

--- go/src/kanzi/util/IntBTree.go
+++ go/src/kanzi/util/IntBTree.go
@@ -20,7 +20,7 @@ import (
 )
 
 // A tree based collection of sorted integers allowing log n time for add/remove
-// and fast access to minimum and maximum values (almost always constant time).
+// and fast access to minimum and maximum values (usually constant time, else log n time).
 
 const (
 	MAX_DIRTY = uint8(1)
@@ -46,9 +46,11 @@ type IntBTree struct {
 	max   int
 }
 
+// Visitor pattern. Must return the node value
+type IntBTreeCallback func(node *IntBTNode) int
+
 func NewIntBTree() (*IntBTree, error) {
 	this := new(IntBTree)
-	this.flags = MIN_DIRTY | MAX_DIRTY
 	return this, nil
 }
 
@@ -61,111 +63,164 @@ func (this *IntBTree) Add(val int) {
 
 	if this.root == nil {
 		this.root = node
-	} else {
-		this.scanAndAdd(this.root, node)
-	}
-
-	if val < this.min {
 		this.min = val
-	}
-
-	if val > this.max {
 		this.max = val
+	} else {
+		addNode(this.root, node)
+
+		if val < this.min {
+			this.min = val
+		}
+
+		if val > this.max {
+			this.max = val
+		}
 	}
 
 	this.size++
 }
 
-func (this *IntBTree) scanAndAdd(parent, node *IntBTNode) {
-	if parent == nil {
-		return
+func addNode(parent, node *IntBTNode) {
+	value := node.value
+
+	for value != parent.value {
+		if value < parent.value {
+			if parent.left == nil {
+				parent.left = node
+				return
+			}
+
+			parent = parent.left
+		} else {
+			if parent.right == nil {
+				parent.right = node
+				return
+			}
+
+			parent = parent.right
+		}
 	}
 
-	if node.value < parent.value {
-		if parent.left == nil {
-			parent.left = node
-		} else {
-			this.scanAndAdd(parent.left, node)
-		}
-	} else if node.value > parent.value {
-		if parent.right == nil {
-			parent.right = node
-		} else {
-			this.scanAndAdd(parent.right, node)
-		}
-	} else {
-		parent.count++
-	}
-
+	parent.count++
 }
 
-func (this *IntBTree) Remove(value int) *IntBTNode {
-	if this.root == nil {
-		return nil
+// Return the number of matches
+func (this *IntBTree) Contains(value int) uint {
+	res := findNode(this.root, value)
+
+	if res == nil {
+		return 0
 	}
 
-	res := this.scanAndRemove(this.root, nil, value, true)
-
-	if res == this.root && this.root.count == 0 {
-		if res.right != nil {
-			this.root = res.right
-		} else if res.left != nil {
-			this.root = res.left
-		} else {
-			this.root = nil
-		}
-	}
-
-	if res != nil {
-		if this.root == nil {
-			this.size = 0
-		} else {
-			this.size--
-		}
-
-		// Force recomputation of cached fields
-		if this.min == value {
-			this.flags |= MIN_DIRTY
-		}
-
-		if this.max == value {
-			this.flags |= MAX_DIRTY
-		}
-
-	}
-
-	return res
+	return res.count
 }
 
-func (this *IntBTree) scanAndRemove(current, prev *IntBTNode, value int, right bool) *IntBTNode {
+func findNode(current *IntBTNode, value int) *IntBTNode {
 	if current == nil {
 		return nil
 	}
 
-	if value < current.value {
-		if current.left == nil {
-			return nil
+	for value != current.value {
+		if value < current.value {
+			if current.left == nil {
+				return nil
+			}
+
+			current = current.left
 		} else {
-			return this.scanAndRemove(current.left, current, value, false)
-		}
-	} else if value > current.value {
-		if current.right == nil {
-			return nil
-		} else {
-			return this.scanAndRemove(current.right, current, value, true)
+			if current.right == nil {
+				return nil
+			}
+
+			current = current.right
 		}
 	}
 
+	return current
+}
+
+func (this *IntBTree) Remove(value int) bool {
+	if this.root == nil {
+		return false
+	}
+
+	res := this.removeNode(value)
+
+	if res == nil {
+		return false
+	}
+
+	this.size--
+
+	// Force recomputation of cached fields
+	if this.min == value {
+		this.flags |= MIN_DIRTY
+	}
+
+	if this.max == value {
+		this.flags |= MAX_DIRTY
+	}
+
+	return true
+}
+
+func (this *IntBTree) removeNode(value int) *IntBTNode {
+	current := this.root
+	var prev *IntBTNode
+
+	for value != current.value {
+		if value < current.value {
+			if current.left == nil {
+				return nil
+			}
+
+			prev = current
+			current = current.left
+		} else {
+			if current.right == nil {
+				return nil
+			}
+
+			prev = current
+			current = current.right
+		}
+	}
+
+	// Found target
 	current.count--
 
-	if current.count == 0 && prev != nil {
-		if right {
+	if current.count != 0 {
+		return current
+	}
+
+	if current == this.root {
+		// First, try easy substitutions of root
+		if current.right == nil {
+			this.root = current.left
+		} else if current.left == nil {
+			this.root = current.right
+		} else if value&1 == 0 { // random choice or left or right
+			this.root = current.right
+
+			// Re-insert left branch
+			addNode(this.root, current.left)
+		} else {
+			this.root = current.left
+
+			// Re-insert right branch
+			addNode(this.root, current.right)
+		}
+	}
+
+	if prev != nil {
+		// Remove current node from previous node
+		if prev.right == current {
 			if current.left != nil {
 				prev.right = current.left
 
 				if current.right != nil {
 					// Re-insert right branch
-					this.scanAndAdd(this.root, current.right)
+					addNode(prev, current.right)
 				}
 			} else {
 				prev.right = current.right
@@ -175,7 +230,7 @@ func (this *IntBTree) scanAndRemove(current, prev *IntBTNode, value int, right b
 
 			if current.right != nil {
 				// Re-insert right branch
-				this.scanAndAdd(this.root, current.right)
+				addNode(prev, current.right)
 			}
 		}
 	}
@@ -183,44 +238,51 @@ func (this *IntBTree) scanAndRemove(current, prev *IntBTNode, value int, right b
 	return current
 }
 
-func (this *IntBTree) Scan(callback func(node *IntBTNode), reverse bool) {
-	if callback == nil {
-		return
+func (this *IntBTree) Scan(callback IntBTreeCallback, reverse bool) []int {
+	if callback == nil || this.root == nil {
+		return nil
 	}
 
-	scanAndCall(this.root, callback, reverse) // visitor pattern
+	res := make([]int, this.size)
+	scanAndCall(this.root, res, 0, callback, reverse) // visitor pattern
+	return res
 }
 
-func scanAndCall(current *IntBTNode, callback func(node *IntBTNode), reverse bool) {
-	if current == nil {
-		return
-	}
-
+func scanAndCall(current *IntBTNode, array []int, index int, callback IntBTreeCallback, reverse bool) int {
 	if reverse == false {
 		if current.left != nil {
-			scanAndCall(current.left, callback, reverse)
+			index = scanAndCall(current.left, array, index, callback, false)
 		}
 
 		for i := current.count; i > 0; i-- {
-			callback(current)
+			array[index] = callback(current)
+			index++
 		}
 
 		if current.right != nil {
-			scanAndCall(current.right, callback, reverse)
+			index = scanAndCall(current.right, array, index, callback, false)
 		}
 	} else {
 		if current.right != nil {
-			scanAndCall(current.right, callback, reverse)
+			index = scanAndCall(current.right, array, index, callback, true)
 		}
 
 		for i := current.count; i > 0; i-- {
-			callback(current)
+			array[index] = callback(current)
+			index++
 		}
 
 		if current.left != nil {
-			scanAndCall(current.left, callback, reverse)
+			index = scanAndCall(current.left, array, index, callback, true)
 		}
 	}
+
+	return index
+}
+
+func (this *IntBTree) Clear() {
+	this.root = nil
+	this.size = 0
 }
 
 func (this *IntBTree) Min() (int, error) {
@@ -228,22 +290,19 @@ func (this *IntBTree) Min() (int, error) {
 		return 0, errors.New("Tree is empty")
 	}
 
-	if this.flags&MIN_DIRTY == 0 {
-		return this.min, nil
+	if this.flags&MIN_DIRTY != 0 {
+		// Dynamically scan tree to leftmost position
+		node := this.root
+
+		for node.left != nil {
+			node = node.left
+		}
+
+		this.min = node.value
+		this.flags &= ^MIN_DIRTY
 	}
 
-	// Dynamically scan tree to leftmost position
-	node := this.root
-	minimum := node.value
-
-	for node != nil {
-		minimum = node.value
-		node = node.left
-	}
-
-	this.min = minimum
-	this.flags &= ^MIN_DIRTY
-	return minimum, nil
+	return this.min, nil
 }
 
 func (this *IntBTree) Max() (int, error) {
@@ -251,20 +310,35 @@ func (this *IntBTree) Max() (int, error) {
 		return 0, errors.New("Tree is empty")
 	}
 
-	if this.flags&MAX_DIRTY == 0 {
-		return this.max, nil
+	if this.flags&MAX_DIRTY != 0 {
+		// Dynamically scan tree to rightmost position
+		node := this.root
+
+		for node.right != nil {
+			node = node.right
+		}
+
+		this.max = node.value
+		this.flags &= ^MAX_DIRTY
 	}
 
-	// Dynamically scan tree to rightmost position
-	node := this.root
-	maximum := node.value
+	return this.max, nil
+}
 
-	for node != nil {
-		maximum = node.value
-		node = node.right
+func (this *IntBTree) ToArray(array []int) []int {
+	if this.root == nil {
+		return make([]int, 0)
 	}
 
-	this.max = maximum
-	this.flags &= ^MAX_DIRTY
-	return maximum, nil
+	if array == nil || len(array) < this.size {
+		array = make([]int, this.size)
+	}
+
+	res := array
+	scanAndCall(this.root, res, 0, emptyCallback, false)
+	return res
+}
+
+func emptyCallback(node *IntBTNode) int {
+	return node.value
 }
--- java/src/kanzi/util/IntBTree.java
+++ java/src/kanzi/util/IntBTree.java
@@ -16,7 +16,7 @@ package kanzi.util;
 
 
 // A tree based collection of sorted integers allowing log n time for add/remove 
-// and fast access to minimum and maximum values (almost always constant time). 
+// and fast access to minimum and maximum values (usually constant time, else log n time). 
 // Not thread safe.
 public final class IntBTree 
 {
@@ -32,7 +32,6 @@ public final class IntBTree
    
    public IntBTree() 
    {
-      this.flags = MIN_DIRTY | MAX_DIRTY;
    }
 
    
@@ -47,104 +46,174 @@ public final class IntBTree
       IntBTNode node = new IntBTNode(val, 1);
 
       if (this.root == null) 
+      {
          this.root = node;
-      else
-         this.scanAndAdd(this.root, node);
-
-      if (val < this.min)
          this.min = val;
-      
-      if (val > this.max)
          this.max = val;
-      
+      }
+      else
+      {
+         addNode(this.root, node);
+
+         if (val < this.min)
+            this.min = val;
+
+         if (val > this.max)
+            this.max = val;
+      }
+   
       this.size++;
    }
 
    
-   private void scanAndAdd(IntBTNode parent, IntBTNode node) 
+   private static void addNode(IntBTNode parent, IntBTNode node) 
    {
-      if (parent == null) 
-         return;
+      final int value = node.value;
+      
+      while (value != parent.value)
+      {
+         if (value < parent.value) 
+         {
+            if (parent.left == null)
+            {
+               parent.left = node;
+               return;
+            }
+            
+            parent = parent.left;
+         } 
+         else
+         {
+            if (parent.right == null)
+            {
+               parent.right = node;
+               return;
+            }
+            
+            parent = parent.right;
+         }
+      }
 
-      if (node.value < parent.value) 
-      {
-         if (parent.left == null)
-            parent.left = node;
-         else
-            this.scanAndAdd(parent.left, node);
-      } 
-      else if (node.value > parent.value) 
-      {
-         if (parent.right == null)
-            parent.right = node;
-         else
-            this.scanAndAdd(parent.right, node);
-      }
-      else 
-      {
-         parent.count++;
-      }
+      parent.count++;
    }
 
    
-   public IntBTNode remove(int value) 
+   // Return the number of matches
+   public int contains(int value)
    {
-      if (this.root == null)
-         return null;
-
-      IntBTNode res = this.scanAndRemove(this.root, null, value, true);
-
-      if ((res == this.root) && (this.root.count == 0)) 
-      {
-         if (res.right != null)
-            this.root = res.right;
-         else if (res.left != null)
-            this.root = res.left;
-         else
-            this.root = null;
-      }
-
-      if (res != null)
-      {
-         this.size = (this.root == null) ? 0 : this.size - 1;
-      
-         // Force recomputation of cached fields
-         if (this.min == value)         
-            this.flags |= MIN_DIRTY;
-
-         if (this.max == value)         
-            this.flags |= MAX_DIRTY;
-      }
-      
-      return res;
+      IntBTNode res = findNode(this.root, value);      
+      return (res == null) ? 0 : res.count;
    }
-
    
-   private IntBTNode scanAndRemove(IntBTNode current, IntBTNode prev, int value, boolean right) 
+   
+   private static IntBTNode findNode(IntBTNode current, int value) 
    {
       if (current == null)
          return null;
+    
+      while (value != current.value)
+      {
+         if (value < current.value) 
+         {
+            if (current.left == null)
+               return null;
 
-      if (value < current.value) 
-      {
-         if (current.left == null)
-            return null;
+            current = current.left;
+         } 
          else
-            return this.scanAndRemove(current.left, current, value, false);
-      } 
-      else if (value > current.value) 
+         {
+            if (current.right == null)
+               return null;
+
+            current = current.right;
+         }
+      }
+      
+      return current;
+   }
+      
+   
+   public boolean remove(int value) 
+   {
+      if (this.root == null)
+         return false;
+
+      IntBTNode res = this.removeNode(value);
+
+      if (res == null)
+         return false;
+
+      this.size--;
+
+      // Force recomputation of cached fields
+      if (this.min == value)         
+         this.flags |= MIN_DIRTY;
+
+      if (this.max == value)         
+         this.flags |= MAX_DIRTY;
+      
+      return true;
+   }
+
+   
+   private IntBTNode removeNode(int value) 
+   {
+      IntBTNode current = this.root;
+      IntBTNode prev = null;
+      
+      while (value != current.value)
       {
-         if (current.right == null)
-            return null;
+         if (value < current.value) 
+         {
+            if (current.left == null)
+               return null;
+
+            prev = current;
+            current = current.left;
+         } 
          else
-            return this.scanAndRemove(current.right, current, value, true);
+         {
+            if (current.right == null)
+               return null;
+
+            prev = current;
+            current = current.right;
+         }
       }
 
+      // Found target
       current.count--;
 
-      if ((current.count == 0) && (prev != null)) 
+      if (current.count != 0)
+         return current;
+      
+      if (current == this.root)
       {
-         if (right == true) 
+         // First, try easy substitutions of root
+         if (current.right == null)
+            this.root = current.left;
+         else if (current.left == null)
+            this.root = current.right;
+         else if ((value & 1) == 0) // random choice or left or right 
+         {
+            this.root = current.right;
+           
+            // Re-insert left branch
+            addNode(this.root, current.left);
+         }
+         else 
+         {
+            this.root = current.left;
+           
+            // Re-insert right branch
+            addNode(this.root, current.right);
+         }
+      }
+           
+      if (prev != null)
+      {
+         // Remove current node from previous node
+         if (prev.right == current) 
          {
             if (current.left != null) 
             {
@@ -153,7 +222,7 @@ public final class IntBTree
                if (current.right != null) 
                {
                   // Re-insert right branch
-                  this.scanAndAdd(this.root, current.right);
+                  addNode(prev, current.right);
                }
             } 
             else
@@ -166,7 +235,7 @@ public final class IntBTree
             if (current.right != null) 
             {
                // Re-insert right branch
-               this.scanAndAdd(this.root, current.right);
+               addNode(prev, current.right);
             }
          }
       }
@@ -175,67 +244,71 @@ public final class IntBTree
    }
 
    
-   public void scan(Callback cb, boolean reverse) 
+   public int[] scan(Callback cb, boolean reverse) 
    {
-      if (cb == null)
-         return;
+      if ((cb == null) || (this.root == null))
+         return null;
 
-      scanAndCall(this.root, cb, reverse); // visitor pattern
+      int[] res = new int[this.size];
+      scanAndCall(this.root, res, 0, cb, reverse); // visitor pattern
+      return res;
    }
 
    
-   private static void scanAndCall(IntBTNode current, Callback cb, boolean reverse) 
+   private static int scanAndCall(IntBTNode current, int[] array, int index, Callback cb, boolean reverse)
    {
-      if (current == null)
-         return;
-
       if (reverse == false) 
       {
          if (current.left != null)
-            scanAndCall(current.left, cb, reverse);
+            index = scanAndCall(current.left, array, index, cb, false);
 
          for (int i=current.count; i>0; i--)
-            cb.call(current);
+            array[index++] = cb.call(current);
 
          if (current.right != null)
-            scanAndCall(current.right, cb, reverse);
+            index = scanAndCall(current.right, array, index, cb, false);
       } 
       else 
       {
          if (current.right != null)
-            scanAndCall(current.right, cb, reverse);
+            index = scanAndCall(current.right, array, index, cb, true);
 
          for (int i=current.count; i>0; i--)
-            cb.call(current);
+            array[index++] = cb.call(current);
 
          if (current.left != null)
-            scanAndCall(current.left, cb, reverse);
+            index = scanAndCall(current.left, array, index, cb, true);
       }
+      
+      return index;
    }
 
+   
+   public void clear()
+   {
+      this.root = null;
+      this.size = 0;
+   }
+   
    
    public int min()
    {
       if (this.root == null)
          throw new IllegalStateException("Tree is empty");
 
-      if ((this.flags & MIN_DIRTY) == 0)
-         return this.min;
-      
-      // Dynamically scan tree to leftmost position
-      IntBTNode node = this.root;
-      int minimum = node.value;
+      if ((this.flags & MIN_DIRTY) != 0)
+      {             
+         // Dynamically scan tree to leftmost position
+         IntBTNode node = this.root;
 
-      do
-      {
-         minimum = node.value;
-         node = node.left;
+         while (node.left != null)
+            node = node.left;
+
+         this.min = node.value;
+         this.flags &= ~MIN_DIRTY;
       }
-      while (node != null);
       
-      this.min = minimum;
-      this.flags &= ~MIN_DIRTY;
-      return minimum;
+      return this.min;
    }
 
    
@@ -244,30 +317,50 @@ public final class IntBTree
       if (this.root == null)
          throw new IllegalStateException("Tree is empty");
 
-      if ((this.flags & MAX_DIRTY) == 0)
-         return this.max;
- 
-      // Dynamically scan tree to rightmost position
-      IntBTNode node = this.root;
-      int maximum = node.value;
-
-      do 
+      if ((this.flags & MAX_DIRTY) != 0)
       {
-         maximum = node.value;
-         node = node.right;
+         // Dynamically scan tree to rightmost position
+         IntBTNode node = this.root;
+
+         while (node.right != null) 
+            node = node.right;
+
+         this.max = node.value;
+         this.flags &= ~MAX_DIRTY;
       }
-      while (node != null);
       
-      this.max = maximum;
-      this.flags &= ~MAX_DIRTY;
-      return maximum;
+      return this.max;
    }
 
    
-   // Interface to implement visitor pattern
+   public int[] toArray(int[] array)
+   {
+      if (this.root == null)
+         return new int[0];
+
+      if ((array == null) || (array.length < this.size))
+         array = new int[this.size];
+   
+      final int[] res = array;
+      
+      Callback cb = new Callback()
+      {
+         @Override
+         public int call(IntBTNode node) 
+         {
+            return node.value();
+         }        
+      };   
+      
+      scanAndCall(this.root, res, 0, cb, false);
+      return res;
+   }
+   
+   
+   // Interface to implement visitor pattern. Must return the node value
    public interface Callback 
    {
-      public void call(IntBTNode node);
+      public int call(IntBTNode node);
    }
 
   
@@ -291,6 +384,5 @@ public final class IntBTree
       }
    }
    
-   
-  
+
 }
