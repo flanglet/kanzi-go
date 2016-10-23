23f741063d478015e6b0d8836da0a6b1f04ef49c
Frederic Langlet <flanglet@gmail.com>
2016-10-22 21:03:12 -0700

    Rewrite implementation to be cleaner and faster.

--- java/src/kanzi/util/sort/MergeSort.java
+++ java/src/kanzi/util/sort/MergeSort.java
@@ -1,113 +1,116 @@
 /*
-Copyright 2011-2013 Frederic Langlet
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-you may obtain a copy of the License at
+ Copyright 2011-2013 Frederic Langlet
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ you may obtain a copy of the License at
 
-                http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package kanzi.util.sort;
 
-import kanzi.IndexedIntArray;
 import kanzi.IntSorter;
 
 
 // A MergeSort is conceptually very simple (divide and merge) but usually not
-// very performant ... except for almost sorted data
-// This implementation based on OpenJDK avoids the usual trap of many array creations
+// very performant except for almost sorted data
 public class MergeSort implements IntSorter
 {
-    private static final int SMALL_ARRAY_THRESHOLD = 32;
-
-    private int[] buffer;
-
-
-    public MergeSort()
-    {
-       this.buffer = new int[0];
-    }
+   private static final int SMALL_ARRAY_THRESHOLD = 32;
+   
+   private int[] buffer;
+   private final IntSorter insertionSort;
 
 
-    // Not thread safe
-    @Override
-    public boolean sort(int[] input, int blkptr, int len)
-    {
-        if ((blkptr < 0) || (len <= 0) || (blkptr+len > input.length))
-            return false;
+   public MergeSort()
+   {
+      this.buffer = new int[0];
+      this.insertionSort = new InsertionSort();
+   }
 
-        if (len == 1)
-           return true;
-        
-        if (this.buffer.length < len)
-            this.buffer = new int[len];
-
-        System.arraycopy(input, blkptr, this.buffer, 0, len);
-        IndexedIntArray dst = new IndexedIntArray(input, blkptr);
-        IndexedIntArray src = new IndexedIntArray(this.buffer, 0);
-        sort(src, dst, blkptr, blkptr+len);
-        return true;
-    }
-
-    
-    private static void sort(IndexedIntArray srcIba, IndexedIntArray dstIba, int start, int end)
-    {
-        final int length = end - start;
-        final int[] src = srcIba.array;
-        final int[] dst = dstIba.array;
-
-        // Insertion sort on smallest arrays
-        if (length < SMALL_ARRAY_THRESHOLD)
-        {
-            start += dstIba.index;
-            end   += dstIba.index;
-            
-            for (int i=start; i<end; i++)
-            {
-                for (int j=i; (j>start) && (dst[j-1]>dst[j]); j--)
-                {
-                    final int tmp = dst[j-1];
-                    dst[j-1] = dst[j];
-                    dst[j] = tmp;
-                }
-            }
-
-            return;
-        }
-
-        int mid = (start + end) >>> 1;
-        sort(dstIba, srcIba, start, mid);
-        sort(dstIba, srcIba, mid, end);
-        mid += srcIba.index;
+   @Override
+   public boolean sort(int[] data, int start, int count)
+   {
+      if ((data == null) || (count < 0) || (start < 0))
+         return false;
+      
+      if (start+count > data.length)
+         return false;
+      
+      if (count < 2)
+         return true;
  
-        if (src[mid-1] <= src[mid])
-        {
-           System.arraycopy(src, start, dst, start, length);
-           return;
-        }
+      if (this.buffer.length < count)
+          this.buffer = new int[count];
+                 
+      return this.mergesort(data, 0, count-1);
+   }
 
-        final int starti = start + dstIba.index;
-        final int endi = end + dstIba.index;
-        int j = start + srcIba.index;
-        int k = mid;
-        final int endk = end + srcIba.index;
 
-        for (int i=starti; i<endi; i++)
-        {
-            if ((k >= endk) || (j < mid) && (src[j] <= src[k]))
-                dst[i] = src[j++];
-            else
-                dst[i] = src[k++];
-        }
-    }
+   private boolean mergesort(int[] data, int low, int high)
+   {
+      if (low < high)
+      {
+         int count = high - low + 1;
+         
+         // Insertion sort on smallest arrays
+         if (count < SMALL_ARRAY_THRESHOLD)
+            return this.insertionSort.sort(data, low, count);
+         
+         int middle = low + count / 2;
+         this.mergesort(data, low, middle);
+         this.mergesort(data, middle + 1, high);
+         this.merge(data, low, middle, high);
+      }
+      
+      return true;
+   }
+
+
+   private void merge(int[] data, int low, int middle, int high)
+   {
+      int count = high - low + 1;
+      
+      if (count < 16)
+      {
+         for (int ii=low; ii<=high; ii++)
+            this.buffer[ii] = data[ii];
+      }
+      else
+      {
+         System.arraycopy(data, low, this.buffer, low, count);
+      }
+      
+      int i = low;
+      int j = middle + 1;
+      int k = low;
+               
+      while ((i <= middle) && (j <= high))
+      {
+         if (this.buffer[i] <= this.buffer[j])
+            data[k] = this.buffer[i++];
+         else
+            data[k] = this.buffer[j++];
+
+         k++;
+      }
+      
+      count = middle - i + 1;
+      
+      if (count < 16)
+      {
+         while (i <= middle)
+            data[k++] = this.buffer[i++];
+      }
+      else
+      {
+         System.arraycopy(this.buffer, i, data, k, count);
+      }
+   }
 }
-
-			
-
 
