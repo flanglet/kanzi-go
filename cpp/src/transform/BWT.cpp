flanglet/kanzi-go

commit ccc7cea5509774cf73e49bd8f162c0b1e287dd3b

Use int32 in the Go implementation since the pointers are a concatenation of 8 bit values and 24 bit index. Improves speed.

go/src/kanzi/transform/BWT.go
@@ -62,18 +62,18 @@ const (
 // for respective performance of different suffix sorting algorithms.
 
 type BWT struct {
-	buffer1      []int
+	buffer1      []int32
 	buffer2      []byte // Only used for big blocks (size >= 1<<24)
-	buckets      []int
+	buckets      []int32
 	primaryIndex uint
 	saAlgo       *DivSufSort
 }
 
 func NewBWT() (*BWT, error) {
 	this := new(BWT)
-	this.buffer1 = make([]int, 0)  // Allocate empty: only used in inverse
+	this.buffer1 = make([]int32, 0)  // Allocate empty: only used in inverse
 	this.buffer2 = make([]byte, 0) // Allocate empty: only used for big blocks (size >= 1<<24)
-	this.buckets = make([]int, 256)
+	this.buckets = make([]int32, 256)
 	return this, nil
 }
 
@@ -203,7 +203,7 @@ func (this *BWT) Inverse(src, dst []byte) (uint, uint, error) {
 func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, error) {
 	// Lazy dynamic memory allocation
 	if len(this.buffer1) < count {
-		this.buffer1 = make([]int, count)
+		this.buffer1 = make([]int32, count)
 	}
 
 	// Aliasing
@@ -218,23 +218,23 @@ func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, er
 	// Build array of packed index + value (assumes block size < 2^24)
 	// Start with the primary index position
 	pIdx := int(this.PrimaryIndex())
-	val0 := int(src[pIdx])
+	val0 := int32(src[pIdx])
 	data[pIdx] = val0
 	buckets_[val0]++
 
 	for i := 0; i < pIdx; i++ {
-		val := int(src[i])
+		val := int32(src[i])
 		data[i] = (buckets_[val] << 8) | val
 		buckets_[val]++
 	}
 
 	for i := pIdx + 1; i < count; i++ {
-		val := int(src[i])
+		val := int32(src[i])
 		data[i] = (buckets_[val] << 8) | val
 		buckets_[val]++
 	}
 
-	sum := 0
+	sum := int32(0)
 
 	for i := range buckets_ {
 		sum += buckets_[i]
@@ -257,7 +257,7 @@ func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, er
 func (this *BWT) inverseBigBlock(src, dst []byte, count int) (uint, uint, error) {
 	// Lazy dynamic memory allocations
 	if len(this.buffer1) < count {
-		this.buffer1 = make([]int, count)
+		this.buffer1 = make([]int32, count)
 	}
 
 	if len(this.buffer2) < count {
@@ -296,7 +296,7 @@ func (this *BWT) inverseBigBlock(src, dst []byte, count int) (uint, uint, error)
 		buckets_[val]++
 	}
 
-	sum := 0
+	sum := int32(0)
 
 	// Create cumulative histogram
 	for i := range buckets_ {

cpp/Kanzi/transform/BWT.cpp
@@ -1,3 +1,4 @@
+
 /*
 Copyright 2011-2017 Frederic Langlet
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,7 +31,7 @@ bool BWT::setPrimaryIndex(int primaryIndex)
 bool BWT::forward(SliceArray<byte>& input, SliceArray<byte>& output, int count)
 {
     if ((!SliceArray<byte>::isValid(input)) || (!SliceArray<byte>::isValid(output)))
-       return false;
+        return false;
 
     if ((count < 0) || (count + input._index > input._length))
         return false;
@@ -76,7 +77,7 @@ bool BWT::forward(SliceArray<byte>& input, SliceArray<byte>& output, int count)
 bool BWT::inverse(SliceArray<byte>& input, SliceArray<byte>& output, int count)
 {
     if ((!SliceArray<byte>::isValid(input)) || (!SliceArray<byte>::isValid(output)))
-       return false;
+        return false;
 
     if ((count < 0) || (count + input._index > input._length))
         return false;
@@ -138,17 +139,18 @@ bool BWT::inverseRegularBlock(SliceArray<byte>& input, SliceArray<byte>& output,
 
     // Create cumulative histogram
     for (int i = 0, sum = 0; i < 256; i++) {
-        sum += buckets_[i];
-        buckets_[i] = sum - buckets_[i];
+        const int tmp = buckets_[i];
+        buckets_[i] = sum;
+        sum += tmp;
     }
 
     uint ptr = data[pIdx];
-    dst[count - 1] = (byte)ptr;
+    dst[count - 1] = byte(ptr);
 
     // Build inverse
     for (int i = count - 2; i >= 0; i--) {
         ptr = data[(ptr >> 8) + buckets_[ptr & 0xFF]];
-        dst[i] = (byte)ptr;
+        dst[i] = byte(ptr);
     }
 
     input._index += count;

cpp/Kanzi/test/TestBWT.cpp
@@ -60,7 +60,7 @@ void testBWTCorrectness(bool isBWT)
         }
         else {
             for (int i = 0; i < size; i++)
-                buf1[i] = (byte)(65 + (rand() % (4 * ii)));
+                buf1[i] = byte(65 + (rand() % (4 * ii)));
         }
 
         Transform<byte>* bwt;
@@ -139,7 +139,7 @@ int testBWTSpeed(bool isBWT)
          << "BWT Speed test" << endl;
     cout << "Iterations: " << iter << endl;
     cout << "Transform size: " << size << endl;
-    srand((uint)time(nullptr));
+    srand(uint(time(nullptr)));
 
     for (int jj = 0; jj < 3; jj++) {
         byte input[256 * 1024];
@@ -186,22 +186,22 @@ int testBWTSpeed(bool isBWT)
                 }
 
                 if (idx >= 0) {
-                    cout << "Failure at index " << i << " (" << (int)input[i] << "<->" << (int)reverse[i] << ")" << endl;
+                    cout << "Failure at index " << i << " (" << int(input[i]) << "<->" << int(reverse[i]) << ")" << endl;
                     break;
                 }
             }
         }
-            
+
         delete bwt;
 
-        double prod = (double)iter * (double)size;
-        double b2KB = (double)1 / (double)1024;
-        double d1_sec = (double)delta1 / CLOCKS_PER_SEC;
-        double d2_sec = (double)delta2 / CLOCKS_PER_SEC;
-        cout << "Forward transform [ms]: " << (int)(d1_sec * 1000) << endl;
-        cout << "Throughput [KB/s]     : " << (int)(prod * b2KB / d1_sec) << endl;
-        cout << "Reverse transform [ms]: " << (int)(d2_sec * 1000) << endl;
-        cout << "Throughput [KB/s]     : " << (int)(prod * b2KB / d2_sec) << endl;
+        double prod = double(iter) * double(size);
+        double b2KB = double(1) / double(1024);
+        double d1_sec = double(delta1) / CLOCKS_PER_SEC;
+        double d2_sec = double(delta2) / CLOCKS_PER_SEC;
+        cout << "Forward transform [ms] : " << int(d1_sec * 1000) << endl;
+        cout << "Throughput [KB/s]      : " << int(prod * b2KB / d1_sec) << endl;
+        cout << "Reverse transform [ms] : " << int(d2_sec * 1000) << endl;
+        cout << "Throughput [KB/s]      : " << int(prod * b2KB / d2_sec) << endl;
         cout << endl;
     }
 

java/src/kanzi/transform/BWT.java
@@ -232,8 +232,9 @@ public class BWT implements ByteTransform
        // Create cumulative histogram
        for (int i=0, sum=0; i<256; i++)
        {
-          sum += buckets_[i];
-          buckets_[i] = sum - buckets_[i];
+          final int tmp = buckets_[i];
+          buckets_[i] = sum;
+          sum += tmp;
        }
 
        int ptr = data[pIdx];

commit 2444c4da0eeba08d8ca6e16178b48cb7295e48da

Fix issue introduced by commit ccc7cea and leading to a potential 'index out of bounds' in the Go implementation. Optimize the memory use of the C++ implementation (change similar to commit ccc7cea).

cpp/src/transform/BWT.cpp
@@ -111,12 +111,12 @@ bool BWT::inverseRegularBlock(SliceArray<byte>& input, SliceArray<byte>& output,
     if (_bufferSize < count) {
         _bufferSize = count;
         delete[] _buffer1;
-        _buffer1 = new int[_bufferSize];
+        _buffer1 = new uint32[_bufferSize];
     }
 
     // Aliasing
-    int* buckets_ = _buckets;
-    int* data = _buffer1;
+    uint32* buckets_ = _buckets;
+    uint32* data = _buffer1;
 
     // Initialize histogram
     memset(_buckets, 0, sizeof(_buckets));
@@ -124,30 +124,31 @@ bool BWT::inverseRegularBlock(SliceArray<byte>& input, SliceArray<byte>& output,
     // Build array of packed index + value (assumes block size < 2^24)
     // Start with the primary index position
     const int pIdx = getPrimaryIndex();
-    const int val0 = src[pIdx] & 0xFF;
+    const uint32 val0 = src[pIdx] & 0xFF;
     data[pIdx] = val0;
     buckets_[val0]++;
 
     for (int i = 0; i < pIdx; i++) {
-        const int val = src[i] & 0xFF;
+        const uint32 val = src[i] & 0xFF;
         data[i] = (buckets_[val] << 8) | val;
         buckets_[val]++;
     }
 
     for (int i = pIdx + 1; i < count; i++) {
-        const int val = src[i] & 0xFF;
+        const uint32 val = src[i] & 0xFF;
         data[i] = (buckets_[val] << 8) | val;
         buckets_[val]++;
     }
 
+    uint32 sum = 0;
+
     // Create cumulative histogram
     for (int i = 0, sum = 0; i < 256; i++) {
-        const int tmp = buckets_[i];
-        buckets_[i] = sum;
-        sum += tmp;
+        sum += buckets_[i];
+        buckets_[i] = sum - buckets_[i];
     }
 
-    uint ptr = data[pIdx];
+    uint32 ptr = data[pIdx];
     dst[count - 1] = byte(ptr);
 
     // Build inverse
@@ -171,14 +172,14 @@ bool BWT::inverseBigBlock(SliceArray<byte>& input, SliceArray<byte>& output, int
     if (_bufferSize < count) {
         _bufferSize = count;
         delete[] _buffer1;
-        _buffer1 = new int[_bufferSize];
+        _buffer1 = new uint32[_bufferSize];
         delete[] _buffer2;
         _buffer2 = new byte[_bufferSize];
     }
 
     // Aliasing
-    int* buckets_ = _buckets;
-    int* data1 = _buffer1;
+    uint32* buckets_ = _buckets;
+    uint32* data1 = _buffer1;
     byte* data2 = _buffer2;
 
     // Initialize histogram
@@ -206,20 +207,21 @@ bool BWT::inverseBigBlock(SliceArray<byte>& input, SliceArray<byte>& output, int
         buckets_[val & 0xFF]++;
     }
 
+    uint32 sum = 0;
+
     // Create cumulative histogram
-    for (int i = 0, sum = 0; i < 256; i++) {
-        const int tmp = buckets_[i];
-        buckets_[i] = sum;
-        sum += tmp;
+    for (int i = 0; i < 256; i++) {
+        sum += buckets_[i];
+        buckets_[i] = sum - buckets_[i];
     }
 
-    int val1 = data1[pIdx];
+    uint32 val1 = data1[pIdx];
     byte val2 = data2[pIdx];
     dst[count - 1] = val2;
 
     // Build inverse
     for (int i = count - 2; i >= 0; i--) {
-        const int idx = val1 + buckets_[val2 & 0xFF];
+        const uint32 idx = val1 + buckets_[val2 & 0xFF];
         val1 = data1[idx];
         val2 = data2[idx];
         dst[i] = val2;

cpp/src/transform/BWT.hpp
@@ -64,10 +64,10 @@ namespace kanzi
        static const int MAX_BLOCK_SIZE = 1024 * 1024 * 1024; // 1 GB (30 bits)
        static const int BWT_MAX_HEADER_SIZE = 4;
 
-       int* _buffer1;
+       uint32* _buffer1;
        byte* _buffer2;
        int _bufferSize;
-       int _buckets[256];
+       uint32 _buckets[256];
        int _primaryIndex;
        DivSufSort _saAlgo;
 
@@ -78,7 +78,7 @@ namespace kanzi
    public:
        BWT()
        {
-           _buffer1 = new int[0]; // Allocate empty: only used in inverse
+           _buffer1 = new uint32[0]; // Allocate empty: only used in inverse
            _buffer2 = new byte[0]; // Allocate empty: only used for big blocks (size >= 1<<24)
            _bufferSize = 0;
        }

go/src/kanzi/transform/BWT.go
@@ -62,18 +62,18 @@ const (
 // for respective performance of different suffix sorting algorithms.
 
 type BWT struct {
-	buffer1      []int32
+	buffer1      []uint32
 	buffer2      []byte // Only used for big blocks (size >= 1<<24)
-	buckets      []int32
+	buckets      []uint32
 	primaryIndex uint
 	saAlgo       *DivSufSort
 }
 
 func NewBWT() (*BWT, error) {
 	this := new(BWT)
-	this.buffer1 = make([]int32, 0)  // Allocate empty: only used in inverse
-	this.buffer2 = make([]byte, 0) // Allocate empty: only used for big blocks (size >= 1<<24)
-	this.buckets = make([]int32, 256)
+	this.buffer1 = make([]uint32, 0) // Allocate empty: only used in inverse
+	this.buffer2 = make([]byte, 0)   // Allocate empty: only used for big blocks (size >= 1<<24)
+	this.buckets = make([]uint32, 256)
 	return this, nil
 }
 
@@ -208,7 +208,7 @@ func (this *BWT) Inverse(src, dst []byte) (uint, uint, error) {
 func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, error) {
 	// Lazy dynamic memory allocation
 	if len(this.buffer1) < count {
-		this.buffer1 = make([]int32, count)
+		this.buffer1 = make([]uint32, count)
 	}
 
 	// Aliasing
@@ -223,23 +223,23 @@ func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, er
 	// Build array of packed index + value (assumes block size < 2^24)
 	// Start with the primary index position
 	pIdx := int(this.PrimaryIndex())
-	val0 := int32(src[pIdx])
+	val0 := uint32(src[pIdx])
 	data[pIdx] = val0
 	buckets_[val0]++
 
 	for i := 0; i < pIdx; i++ {
-		val := int32(src[i])
+		val := uint32(src[i])
 		data[i] = (buckets_[val] << 8) | val
 		buckets_[val]++
 	}
 
 	for i := pIdx + 1; i < count; i++ {
-		val := int32(src[i])
+		val := uint32(src[i])
 		data[i] = (buckets_[val] << 8) | val
 		buckets_[val]++
 	}
 
-	sum := int32(0)
+	sum := uint32(0)
 
 	for i := range buckets_ {
 		sum += buckets_[i]
@@ -262,7 +262,7 @@ func (this *BWT) inverseRegularBlock(src, dst []byte, count int) (uint, uint, er
 func (this *BWT) inverseBigBlock(src, dst []byte, count int) (uint, uint, error) {
 	// Lazy dynamic memory allocations
 	if len(this.buffer1) < count {
-		this.buffer1 = make([]int32, count)
+		this.buffer1 = make([]uint32, count)
 	}
 
 	if len(this.buffer2) < count {
@@ -301,7 +301,7 @@ func (this *BWT) inverseBigBlock(src, dst []byte, count int) (uint, uint, error)
 		buckets_[val]++
 	}
 
-	sum := int32(0)
+	sum := uint32(0)
 
 	// Create cumulative histogram
 	for i := range buckets_ {