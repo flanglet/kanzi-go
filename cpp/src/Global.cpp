4216f2bdc4a8175c0b4b870682ac859367be2e69
Frederic Langlet <flanglet@gmail.com>
2017-12-24 16:15:45 -0800

    Fix compilation error on Linux with g++.

--- cpp/src/Global.cpp
+++ cpp/src/Global.cpp
@@ -13,7 +13,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#ifdef _MSC_VER
 #include <intrin.h>  
+#endif
+
 #include "Global.hpp"
 #include "IllegalArgumentException.hpp"
 
