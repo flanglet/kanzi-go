ea95a94aaead37713ebc73b05685341943d014e0
Frederic Langlet <flanglet@gmail.com>
2018-02-18 17:34:51 -0800

    Format: fix comment

--- java/src/main/java/kanzi/util/hash/MurMurHash3.java
+++ java/src/main/java/kanzi/util/hash/MurMurHash3.java
@@ -15,10 +15,9 @@ limitations under the License.
 
 package kanzi.util.hash;
 
-// MurmurHash3 was written by Austin Appleby, and is placed in the public
-
 import kanzi.Memory;
 
+// MurmurHash3 was written by Austin Appleby, and is placed in the public
 // domain. The author hereby disclaims copyright to this source code.
 // Original source code: https://github.com/aappleby/smhasher
 
