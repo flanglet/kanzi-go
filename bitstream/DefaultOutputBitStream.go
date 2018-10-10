e922b4473904628a138d1766766150a1e64f2c5d
Frederic Langlet <flanglet@gmail.com>
2018-10-10 16:45:05 -0700

    Micro optimization

--- bitstream/DefaultOutputBitStream.go
+++ bitstream/DefaultOutputBitStream.go
@@ -32,6 +32,74 @@ type DefaultOutputBitStream struct {
 	buffer    []byte
 }
 
+var OBS_MASKS = [...]uint64{
+	0x0,
+	0x1,
+	0x3,
+	0x7,
+	0xF,
+	0x1F,
+	0x3F,
+	0x7F,
+	0xFF,
+	0x1FF,
+	0x3FF,
+	0x7FF,
+	0xFFF,
+	0x1FFF,
+	0x3FFF,
+	0x7FFF,
+	0xFFFF,
+	0x1FFFF,
+	0x3FFFF,
+	0x7FFFF,
+	0xFFFFF,
+	0x1FFFFF,
+	0x3FFFFF,
+	0x7FFFFF,
+	0xFFFFFF,
+	0x1FFFFFF,
+	0x3FFFFFF,
+	0x7FFFFFF,
+	0xFFFFFFF,
+	0x1FFFFFFF,
+	0x3FFFFFFF,
+	0x7FFFFFFF,
+	0xFFFFFFFF,
+	0x1FFFFFFFF,
+	0x3FFFFFFFF,
+	0x7FFFFFFFF,
+	0xFFFFFFFFF,
+	0x1FFFFFFFFF,
+	0x3FFFFFFFFF,
+	0x7FFFFFFFFF,
+	0xFFFFFFFFFF,
+	0x1FFFFFFFFFF,
+	0x3FFFFFFFFFF,
+	0x7FFFFFFFFFF,
+	0xFFFFFFFFFFF,
+	0x1FFFFFFFFFFF,
+	0x3FFFFFFFFFFF,
+	0x7FFFFFFFFFFF,
+	0xFFFFFFFFFFFF,
+	0x1FFFFFFFFFFFF,
+	0x3FFFFFFFFFFFF,
+	0x7FFFFFFFFFFFF,
+	0xFFFFFFFFFFFFF,
+	0x1FFFFFFFFFFFFF,
+	0x3FFFFFFFFFFFFF,
+	0x7FFFFFFFFFFFFF,
+	0xFFFFFFFFFFFFFF,
+	0x1FFFFFFFFFFFFFF,
+	0x3FFFFFFFFFFFFFF,
+	0x7FFFFFFFFFFFFFF,
+	0xFFFFFFFFFFFFFFF,
+	0x1FFFFFFFFFFFFFFF,
+	0x3FFFFFFFFFFFFFFF,
+	0x7FFFFFFFFFFFFFFF,
+	0xFFFFFFFFFFFFFFFF,
+}
+
 func NewDefaultOutputBitStream(stream io.WriteCloser, bufferSize uint) (*DefaultOutputBitStream, error) {
 	if stream == nil {
 		return nil, errors.New("Invalid null output stream parameter")
@@ -53,6 +121,7 @@ func NewDefaultOutputBitStream(stream io.WriteCloser, bufferSize uint) (*Default
 	this.buffer = make([]byte, bufferSize)
 	this.os = stream
 	this.availBits = 64
+
 	return this, nil
 }
 
@@ -75,7 +144,7 @@ func (this *DefaultOutputBitStream) WriteBits(value uint64, count uint) uint {
 		panic(fmt.Errorf("Invalid length: %v (must be in [1..64])", count))
 	}
 
-	value &= (0xFFFFFFFFFFFFFFFF >> (64 - count))
+	value &= OBS_MASKS[count]
 	bi := uint(this.availBits)
 
 	// Pad the current position in buffer
