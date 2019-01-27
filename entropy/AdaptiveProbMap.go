838493bfb80e01520442c17b1c492855cb3f48a4
Frederic Langlet <flanglet@gmail.com>
2019-01-26 21:57:53 -0800

    Reduce memory usage

--- entropy/AdaptiveProbMap.go
+++ entropy/AdaptiveProbMap.go
@@ -24,9 +24,9 @@ import (
 // its state to improve future guesses.
 
 type AdaptiveProbMap struct {
-	index int     // last prob, context
-	rate  uint    // update rate
-	data  []int32 // prob, context -> prob
+	index int      // last prob, context
+	rate  uint     // update rate
+	data  []uint16 // prob, context -> prob
 }
 
 type LinearAdaptiveProbMap AdaptiveProbMap
@@ -35,11 +35,11 @@ type FastLogisticAdaptiveProbMap AdaptiveProbMap
 
 func newLogisticAdaptiveProbMap(n, rate uint) (*LogisticAdaptiveProbMap, error) {
 	this := &LogisticAdaptiveProbMap{}
-	this.data = make([]int32, n*33)
+	this.data = make([]uint16, n*33)
 	this.rate = rate
 
 	for j := 0; j <= 32; j++ {
-		this.data[j] = int32(kanzi.Squash((j-16)<<7) << 4)
+		this.data[j] = uint16(kanzi.Squash((j-16)<<7) << 4)
 	}
 
 	for i := uint(1); i < n; i++ {
@@ -52,26 +52,26 @@ func newLogisticAdaptiveProbMap(n, rate uint) (*LogisticAdaptiveProbMap, error) 
 // Return improved prediction given current bit, prediction and context
 func (this *LogisticAdaptiveProbMap) get(bit int, pr int, ctx int) int {
 	// Update probability based on error and learning rate
-	g := int32((-bit & 65528) + (bit << this.rate))
-	this.data[this.index+1] += ((g - this.data[this.index+1]) >> this.rate)
-	this.data[this.index] += ((g - this.data[this.index]) >> this.rate)
+	g := (-bit & 65528) + (bit << this.rate)
+	this.data[this.index+1] += uint16((g - int(this.data[this.index+1])) >> this.rate)
+	this.data[this.index] += uint16((g - int(this.data[this.index])) >> this.rate)
 	pr = kanzi.STRETCH[pr]
 
 	// Find index: 33*ctx + quantized prediction in [0..32]
 	this.index = ((pr + 2048) >> 7) + 33*ctx
 
 	// Return interpolated probability
-	w := int32(pr & 127)
-	return int(this.data[this.index+1]*w+this.data[this.index]*(128-w)) >> 11
+	w := pr & 127
+	return (int(this.data[this.index+1])*w + int(this.data[this.index])*(128-w)) >> 11
 }
 
 func newFastLogisticAdaptiveProbMap(n, rate uint) (*FastLogisticAdaptiveProbMap, error) {
 	this := &FastLogisticAdaptiveProbMap{}
-	this.data = make([]int32, n*33)
+	this.data = make([]uint16, n*33)
 	this.rate = rate
 
 	for j := 0; j <= 32; j++ {
-		this.data[j] = int32(kanzi.Squash((j-16)<<7) << 4)
+		this.data[j] = uint16(kanzi.Squash((j-16)<<7) << 4)
 	}
 
 	for i := uint(1); i < n; i++ {
@@ -84,19 +84,19 @@ func newFastLogisticAdaptiveProbMap(n, rate uint) (*FastLogisticAdaptiveProbMap,
 // Return improved prediction given current bit, prediction and context
 func (this *FastLogisticAdaptiveProbMap) get(bit int, pr int, ctx int) int {
 	// Update probability based on error and learning rate
-	g := int32((-bit & 65528) + (bit << this.rate))
-	this.data[this.index] += ((g - this.data[this.index]) >> this.rate)
+	g := (-bit & 65528) + (bit << this.rate)
+	this.data[this.index] += uint16((g - int(this.data[this.index])) >> this.rate)
 	this.index = ((kanzi.STRETCH[pr] + 2048) >> 7) + 33*ctx
 	return int(this.data[this.index]) >> 4
 }
 
 func newLinearAdaptiveProbMap(n, rate uint) (*LinearAdaptiveProbMap, error) {
 	this := &LinearAdaptiveProbMap{}
-	this.data = make([]int32, n*65)
+	this.data = make([]uint16, n*65)
 	this.rate = rate
 
 	for j := 0; j <= 64; j++ {
-		this.data[j] = int32(j<<6) << 4
+		this.data[j] = uint16(j<<6) << 4
 	}
 
 	for i := uint(1); i < n; i++ {
@@ -109,15 +109,14 @@ func newLinearAdaptiveProbMap(n, rate uint) (*LinearAdaptiveProbMap, error) {
 // Return improved prediction given current bit, prediction and context
 func (this *LinearAdaptiveProbMap) get(bit int, pr int, ctx int) int {
 	// Update probability based on error and learning rate
-	g := int32((-bit & 65528) + (bit << this.rate))
-	this.data[this.index+1] += ((g - this.data[this.index+1]) >> this.rate)
-	this.data[this.index] += ((g - this.data[this.index]) >> this.rate)
-	pr = kanzi.STRETCH[pr]
+	g := (-bit & 65528) + (bit << this.rate)
+	this.data[this.index+1] += uint16((g - int(this.data[this.index+1])) >> this.rate)
+	this.data[this.index] += uint16((g - int(this.data[this.index])) >> this.rate)
 
 	// Find index: 65*ctx + quantized prediction in [0..64]
 	this.index = (pr >> 6) + 65*ctx
 
 	// Return interpolated probability
-	w := int32(pr & 127)
-	return int(this.data[this.index+1]*w+this.data[this.index]*(128-w)) >> 11
+	w := pr & 127
+	return (int(this.data[this.index+1])*w + int(this.data[this.index])*(128-w)) >> 11
 }
