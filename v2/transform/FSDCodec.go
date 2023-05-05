d0914e1d58ca5fcd525494c1c15017be687a110a
Frederic Langlet <flanglet@gmail.com>
2023-05-05 15:39:33 +0200

    Add shift 16 check and other optimizations

--- v2/transform/FSDCodec.go
+++ v2/transform/FSDCodec.go
@@ -29,6 +29,76 @@ const (
 	_FSD_XOR_CODING       = byte(1)
 )
 
+var _FSD_ZIGZAG1 = [256]byte{
+	253, 251, 249, 247, 245, 243, 241, 239,
+	237, 235, 233, 231, 229, 227, 225, 223,
+	221, 219, 217, 215, 213, 211, 209, 207,
+	205, 203, 201, 199, 197, 195, 193, 191,
+	189, 187, 185, 183, 181, 179, 177, 175,
+	173, 171, 169, 167, 165, 163, 161, 159,
+	157, 155, 153, 151, 149, 147, 145, 143,
+	141, 139, 137, 135, 133, 131, 129, 127,
+	125, 123, 121, 119, 117, 115, 113, 111,
+	109, 107, 105, 103, 101, 99, 97, 95,
+	93, 91, 89, 87, 85, 83, 81, 79,
+	77, 75, 73, 71, 69, 67, 65, 63,
+	61, 59, 57, 55, 53, 51, 49, 47,
+	45, 43, 41, 39, 37, 35, 33, 31,
+	29, 27, 25, 23, 21, 19, 17, 15,
+	13, 11, 9, 7, 5, 3, 1, 0,
+	2, 4, 6, 8, 10, 12, 14, 16,
+	18, 20, 22, 24, 26, 28, 30, 32,
+	34, 36, 38, 40, 42, 44, 46, 48,
+	50, 52, 54, 56, 58, 60, 62, 64,
+	66, 68, 70, 72, 74, 76, 78, 80,
+	82, 84, 86, 88, 90, 92, 94, 96,
+	98, 100, 102, 104, 106, 108, 110, 112,
+	114, 116, 118, 120, 122, 124, 126, 128,
+	130, 132, 134, 136, 138, 140, 142, 144,
+	146, 148, 150, 152, 154, 156, 158, 160,
+	162, 164, 166, 168, 170, 172, 174, 176,
+	178, 180, 182, 184, 186, 188, 190, 192,
+	194, 196, 198, 200, 202, 204, 206, 208,
+	210, 212, 214, 216, 218, 220, 222, 224,
+	226, 228, 230, 232, 234, 236, 238, 240,
+	242, 244, 246, 248, 250, 252, 254, 255,
+}
+
+var _FSD_ZIGZAG2 = [256]int{
+	0, -1, 1, -2, 2, -3, 3, -4,
+	4, -5, 5, -6, 6, -7, 7, -8,
+	8, -9, 9, -10, 10, -11, 11, -12,
+	12, -13, 13, -14, 14, -15, 15, -16,
+	16, -17, 17, -18, 18, -19, 19, -20,
+	20, -21, 21, -22, 22, -23, 23, -24,
+	24, -25, 25, -26, 26, -27, 27, -28,
+	28, -29, 29, -30, 30, -31, 31, -32,
+	32, -33, 33, -34, 34, -35, 35, -36,
+	36, -37, 37, -38, 38, -39, 39, -40,
+	40, -41, 41, -42, 42, -43, 43, -44,
+	44, -45, 45, -46, 46, -47, 47, -48,
+	48, -49, 49, -50, 50, -51, 51, -52,
+	52, -53, 53, -54, 54, -55, 55, -56,
+	56, -57, 57, -58, 58, -59, 59, -60,
+	60, -61, 61, -62, 62, -63, 63, -64,
+	64, -65, 65, -66, 66, -67, 67, -68,
+	68, -69, 69, -70, 70, -71, 71, -72,
+	72, -73, 73, -74, 74, -75, 75, -76,
+	76, -77, 77, -78, 78, -79, 79, -80,
+	80, -81, 81, -82, 82, -83, 83, -84,
+	84, -85, 85, -86, 86, -87, 87, -88,
+	88, -89, 89, -90, 90, -91, 91, -92,
+	92, -93, 93, -94, 94, -95, 95, -96,
+	96, -97, 97, -98, 98, -99, 99, -100,
+	100, -101, 101, -102, 102, -103, 103, -104,
+	104, -105, 105, -106, 106, -107, 107, -108,
+	108, -109, 109, -110, 110, -111, 111, -112,
+	112, -113, 113, -114, 114, -115, 115, -116,
+	116, -117, 117, -118, 118, -119, 119, -120,
+	120, -121, 121, -122, 122, -123, 123, -124,
+	124, -125, 125, -126, 126, -127, 127, -128,
+}
+
 // FSDCodec Fixed Step Delta codec is used to decorrelate values separated
 // by a constant distance (step) and encode residuals
 type FSDCodec struct {
@@ -93,8 +163,6 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 		}
 	}
 
-	dstEnd := this.MaxEncodedLen(count)
-	var histo [6][256]int
 	magic := kanzi.GetMagicType(src)
 
 	// Skip detection except for a few candidate types
@@ -119,39 +187,34 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 	// Sample 2 sub-blocks
 	count5 := count / 5
 	count10 := count / 10
-	start := 3 * count5
-	end := start + count10
+	in1 := src[1*count5-16 : 1*count5+count10-16]
+	in2 := src[3*count5-16 : 3*count5+count10-16]
+	var histo [7][256]int
 
-	for i := start; i < end; i++ {
-		b := src[i]
-		histo[0][b]++
-		histo[1][b^src[i-1]]++
-		histo[2][b^src[i-2]]++
-		histo[3][b^src[i-3]]++
-		histo[4][b^src[i-4]]++
-		histo[5][b^src[i-8]]++
-	}
-
-	start = 1 * count5
-	end = start + count10
-
-	for i := start; i < end; i++ {
-		b := src[i]
-		histo[0][b]++
-		histo[1][b^src[i-1]]++
-		histo[2][b^src[i-2]]++
-		histo[3][b^src[i-3]]++
-		histo[4][b^src[i-4]]++
-		histo[5][b^src[i-8]]++
+	for i := 16; i < count10; i++ {
+		b1 := in1[i]
+		histo[0][b1]++
+		histo[1][b1^in1[i-1]]++
+		histo[2][b1^in1[i-2]]++
+		histo[3][b1^in1[i-3]]++
+		histo[4][b1^in1[i-4]]++
+		histo[5][b1^in1[i-8]]++
+		histo[6][b1^in1[i-16]]++
+		b2 := in2[i]
+		histo[0][b2]++
+		histo[1][b2^in2[i-1]]++
+		histo[2][b2^in2[i-2]]++
+		histo[3][b2^in2[i-3]]++
+		histo[4][b2^in2[i-4]]++
+		histo[5][b2^in2[i-8]]++
+		histo[6][b2^in2[i-16]]++
 	}
 
 	// Find if entropy is lower post transform
-	var ent [6]int
-	ent[0] = kanzi.ComputeFirstOrderEntropy1024(count5, histo[0][:])
-
+	var ent [7]int
 	minIdx := 0
 
-	for i := 1; i < len(ent); i++ {
+	for i := 0; i < len(ent); i++ {
 		ent[i] = kanzi.ComputeFirstOrderEntropy1024(count5, histo[i][:])
 
 		if ent[i] < ent[minIdx] {
@@ -159,12 +222,12 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 		}
 	}
 
-	if this.ctx != nil {
-		(*this.ctx)["dataType"] = kanzi.DetectSimpleType(count5, histo[0][:])
-	}
-
 	// If not better, quick exit
 	if ent[minIdx] >= ent[0] {
+		if this.ctx != nil {
+			(*this.ctx)["dataType"] = kanzi.DetectSimpleType(count5, histo[0][:])
+		}
+
 		return 0, 0, fmt.Errorf("FSD forward transform skip")
 	}
 
@@ -172,12 +235,8 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 		(*this.ctx)["dataType"] = kanzi.DT_MULTIMEDIA
 	}
 
-	dist := minIdx
-
-	if dist > 4 {
-		dist = 8
-	}
-
+	var distances = []int{0, 1, 2, 3, 4, 8, 16}
+	dist := distances[minIdx]
 	largeDeltas := 0
 
 	// Detect best coding by sampling for large deltas
@@ -197,6 +256,7 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 		mode = _FSD_XOR_CODING
 	}
 
+	dstEnd := this.MaxEncodedLen(count)
 	dst[0] = mode
 	dst[1] = byte(dist)
 	srcIdx := 0
@@ -215,7 +275,7 @@ func (this *FSDCodec) Forward(src, dst []byte) (uint, uint, error) {
 			delta := int32(src[srcIdx]) - int32(src[srcIdx-dist])
 
 			if delta >= -127 && delta <= 127 {
-				dst[dstIdx] = byte((delta >> 31) ^ (delta << 1)) // zigzag encode delta
+				dst[dstIdx] = _FSD_ZIGZAG1[delta+127] // zigzag encode delta
 				srcIdx++
 				dstIdx++
 				continue
@@ -288,7 +348,7 @@ func (this *FSDCodec) Inverse(src, dst []byte) (uint, uint, error) {
 	dist := int(src[1])
 
 	// Sanity check
-	if (dist < 1) || ((dist > 4) && (dist != 8)) {
+	if (dist < 1) || ((dist > 4) && (dist != 8) && (dist != 16)) {
 		return 0, 0, errors.New("FSD inverse transform failed: invalid data")
 	}
 
@@ -308,8 +368,7 @@ func (this *FSDCodec) Inverse(src, dst []byte) (uint, uint, error) {
 	if mode == _FSD_DELTA_CODING {
 		for srcIdx < srcEnd && dstIdx < dstEnd {
 			if src[srcIdx] != _FSD_ESCAPE_TOKEN {
-				delta := int32(src[srcIdx]>>1) ^ -int32(src[srcIdx]&1) // zigzag decode delta
-				dst[dstIdx] = byte(int32(dst[dstIdx-dist]) + delta)
+				dst[dstIdx] = byte(int(dst[dstIdx-dist]) + _FSD_ZIGZAG2[src[srcIdx]])
 				srcIdx++
 				dstIdx++
 				continue
