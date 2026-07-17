50c68f21c6619f786b1a8a8850a6f3155e4630e6
Frederic Langlet <flanglet@gmail.com>
2026-07-17 13:41:16 -0400

    Flatten CM predictor counters.

--- v2/entropy/CMPredictor.go
+++ v2/entropy/CMPredictor.go
@@ -18,10 +18,12 @@ package entropy
 import "fmt"
 
 const (
-	_CM_FAST_RATE   = 2
-	_CM_MEDIUM_RATE = 4
-	_CM_SLOW_RATE   = 6
-	_CM_PSCALE      = 65536
+	_CM_FAST_RATE       = 2
+	_CM_MEDIUM_RATE     = 4
+	_CM_SLOW_RATE       = 6
+	_CM_PSCALE          = 65536
+	_CM_COUNTER1_STRIDE = 257
+	_CM_COUNTER2_STRIDE = 17
 )
 
 type CMPredictor struct {
@@ -29,8 +31,8 @@ type CMPredictor struct {
 	c2           byte
 	ctx          int32
 	runMask      int32
-	counter1     [256][]int32
-	counter2     [512][]int32
+	counter1     []int32
+	counter2     []int32
 	idx          int
 	isBsVersion3 bool
 }
@@ -54,26 +56,29 @@ func NewCMPredictor(ctx *map[string]any) (*CMPredictor, error) {
 
 	this.isBsVersion3 = bsVersion < 4
 
+	this.counter1 = make([]int32, 256*_CM_COUNTER1_STRIDE)
+	this.counter2 = make([]int32, 512*_CM_COUNTER2_STRIDE)
+
 	for i := 0; i < 256; i++ {
-		this.counter1[i] = make([]int32, 257)
-		this.counter2[i+i] = make([]int32, 17)
-		this.counter2[i+i+1] = make([]int32, 17)
+		counter1Idx := i * _CM_COUNTER1_STRIDE
+		counter2Idx := (i + i) * _CM_COUNTER2_STRIDE
+
 
 		for j := 0; j <= 256; j++ {
-			this.counter1[i][j] = _CM_PSCALE >> 1
+			this.counter1[counter1Idx+j] = _CM_PSCALE >> 1
 		}
 
 		for j := 0; j < 16; j++ {
-			this.counter2[i+i][j] = int32(j << 12)
-			this.counter2[i+i+1][j] = int32(j << 12)
+			this.counter2[counter2Idx+j] = int32(j << 12)
+			this.counter2[counter2Idx+_CM_COUNTER2_STRIDE+j] = int32(j << 12)
 		}
 
 		if this.isBsVersion3 == true {
-			this.counter2[i+i][16] = int32(15 << 12)
-			this.counter2[i+i+1][16] = int32(15 << 12)
+			this.counter2[counter2Idx+16] = int32(15 << 12)
+			this.counter2[counter2Idx+_CM_COUNTER2_STRIDE+16] = int32(15 << 12)
 		} else {
-			this.counter2[i+i][16] = 65535
-			this.counter2[i+i+1][16] = 65535
+			this.counter2[counter2Idx+16] = 65535
+			this.counter2[counter2Idx+_CM_COUNTER2_STRIDE+16] = 65535
 		}
 	}
 
@@ -82,20 +87,21 @@ func NewCMPredictor(ctx *map[string]any) (*CMPredictor, error) {
 
 // Update updates the probability model based on the internal bit counters
 func (this *CMPredictor) Update(bit byte) {
-	pc2 := this.counter2[this.ctx|this.runMask]
-	pc1 := this.counter1[this.ctx]
+	pc2 := int(this.ctx|this.runMask) * _CM_COUNTER2_STRIDE
+	pc1 := int(this.ctx) * _CM_COUNTER1_STRIDE
+	c1 := pc1 + int(this.c1)
 
 	if bit == 0 {
-		pc1[256] -= (pc1[256] >> _CM_FAST_RATE)
-		pc1[this.c1] -= (pc1[this.c1] >> _CM_MEDIUM_RATE)
-		pc2[this.idx] -= (pc2[this.idx] >> _CM_SLOW_RATE)
-		pc2[this.idx+1] -= (pc2[this.idx+1] >> _CM_SLOW_RATE)
+		this.counter1[pc1+256] -= (this.counter1[pc1+256] >> _CM_FAST_RATE)
+		this.counter1[c1] -= (this.counter1[c1] >> _CM_MEDIUM_RATE)
+		this.counter2[pc2+this.idx] -= (this.counter2[pc2+this.idx] >> _CM_SLOW_RATE)
+		this.counter2[pc2+this.idx+1] -= (this.counter2[pc2+this.idx+1] >> _CM_SLOW_RATE)
 		this.ctx += this.ctx
 	} else {
-		pc1[256] -= ((pc1[256] - _CM_PSCALE + 16) >> _CM_FAST_RATE)
-		pc1[this.c1] -= ((pc1[this.c1] - _CM_PSCALE + 16) >> _CM_MEDIUM_RATE)
-		pc2[this.idx] -= ((pc2[this.idx] - _CM_PSCALE + 16) >> _CM_SLOW_RATE)
-		pc2[this.idx+1] -= ((pc2[this.idx+1] - _CM_PSCALE + 16) >> _CM_SLOW_RATE)
+		this.counter1[pc1+256] -= ((this.counter1[pc1+256] - _CM_PSCALE + 16) >> _CM_FAST_RATE)
+		this.counter1[c1] -= ((this.counter1[c1] - _CM_PSCALE + 16) >> _CM_MEDIUM_RATE)
+		this.counter2[pc2+this.idx] -= ((this.counter2[pc2+this.idx] - _CM_PSCALE + 16) >> _CM_SLOW_RATE)
+		this.counter2[pc2+this.idx+1] -= ((this.counter2[pc2+this.idx+1] - _CM_PSCALE + 16) >> _CM_SLOW_RATE)
 		this.ctx += (this.ctx + 1)
 	}
 
@@ -116,12 +122,12 @@ func (this *CMPredictor) Update(bit byte) {
 // in the [0..4095] range. The probability is computed from the internal
 // bit counters.
 func (this *CMPredictor) Get() int {
-	pc2 := this.counter2[this.ctx|this.runMask]
-	pc1 := this.counter1[this.ctx]
-	p := int(13*(pc1[256]+pc1[this.c1])+6*pc1[this.c2]) >> 5
+	pc2 := int(this.ctx|this.runMask) * _CM_COUNTER2_STRIDE
+	pc1 := int(this.ctx) * _CM_COUNTER1_STRIDE
+	p := int(13*(this.counter1[pc1+256]+this.counter1[pc1+int(this.c1)])+6*this.counter1[pc1+int(this.c2)]) >> 5
 	this.idx = p >> 12
-	x2 := int(pc2[this.idx+1])
-	x1 := int(pc2[this.idx])
+	x2 := int(this.counter2[pc2+this.idx+1])
+	x1 := int(this.counter2[pc2+this.idx])
 
 	if this.isBsVersion3 == true {
 		ssep := x1 + (((x2 - x1) * (p & 4095)) >> 12)
